eef099878bb80adbac913c89a1cfe3ff21f8f745
hasanar-star <Hasan.mohammed@kalpitatechnologies.com>
2022-08-05 16:00:43 +0530

    new project

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public abstract class Bank
+{
+    public virtual void withdraw()
+    {
+        Console.WriteLine("Thank You. Bonus 200 added to your account");
+    }
+}
+
+public class YesBank : Bank
+{
+    public override void withdraw()
+    {
+        Console.WriteLine("Withdrawn cash from YesBank");
+        base.withdraw();
+    }
+}
+
+public class NoBank : Bank
+{
+    public override void withdraw()
+    {
+        Console.WriteLine("Withdrawn cash from NoBank");
+    }
+}
+
+namespace Studytonight
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Bank b = new YesBank();
+            b.withdraw();
+            b = new NoBank();
+            b.withdraw();
+        }
+    }
+}
