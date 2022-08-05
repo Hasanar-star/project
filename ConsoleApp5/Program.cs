0cbbff92a21c79e1d47bbe03ac883e1d627313aa
hasanar-star <Hasan.mohammed@kalpitatechnologies.com>
2022-08-05 16:19:47 +0530

    updated

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Studytonight
+{
+    public class Interest
+    {
+        // interest for 1 year of tenure
+        public double TrueBank(double amount, double rate)
+        {
+            return amount + (amount * rate);
+        }
+        public double TrueBank(double amount, double rate, string holdertype)
+        {
+            return amount + (amount * rate) + 2000;
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Interest i = new Interest();
+
+            double finalamount = i.TrueBank(5000.00, 0.1);
+            Console.WriteLine("Normal interest for a holder " + finalamount);
+
+            finalamount = i.TrueBank(5000.00, 0.1, "prime");
+            Console.WriteLine("Prime interest for a holder " + finalamount);
+
+        }
+    }
+}
